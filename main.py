fdecb160ee4463a91f24fb0b3e78ecdb5871fcc4
Michal Falat <michal.falat@m2ms.sk>
2018-11-27 09:58:58 +0100

    Added floor plan with basic pose estimation in  2d space

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import cv2
 import configuration as configuration
 import cv2.aruco as aruco
 
+class FloorPlan():
+    def __init__(self, pixelSize_ONE_METER ):
+        self.ONE_METER_SCALE = pixelSize_ONE_METER
+    
+    def calculateSizeToPixels(self, centimeters):
+        return self.ONE_METER_SCALE * (centimeters/100)
+
+class ArucoPointPosition:
+    def __init__(self, position, rotation, size = configuration.MARKER_SIZE):
+        self.position = position
+        self.rotation = rotation
+        self.size = size
+
+    def getMarkerLine(self, scale):
+        return ((self.position[0]-40, self.position[1]), (self.position[0]+40, self.position[1]))
+
+    def getMiddlePoint(self):
+        return calculateMiddlePointBetweenPoints(self.getMarkerLine(1)[0], self.getMarkerLine(1)[1])
 
 class ArucoInfo:
     def __init__(self, points, markerId):
@@ -14,7 +32,7 @@ class ArucoInfo:
         self.markerId = markerId
         self.estimatedAngle = None
         self.estimatedDistance = None
-        self.sample50cm = 15
+        self.sample50cm = 12
 
     def getPoints(self):
         return [self.uL, self.uR, self.dL, self.dR]
@@ -50,7 +68,7 @@ def calculateMiddlePointBetweenPoints(p1, p2):
     return PointInfo([x, y])
 
 
-def projectArucoMarker(imgW, imgH, marker=None):
+def projectArucoMarker(imgW, imgH, floor, marker=None):
     img_size = (imgH, imgW, 3)
     img = np.ones(img_size) * 255
     if(marker != None):
@@ -78,33 +96,65 @@ def projectArucoMarker(imgW, imgH, marker=None):
         cv2.putText(img, 'Estimated distance: ' + str(estimatedDistance) + ' cm', (10, 50), font, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
         cv2.putText(img, 'Estimated angle: ' + str(estimatedAngle) + ' degrees', (10, 70), font, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
-    projectArucoPosition(marker)
+    projectArucoPosition(floor, marker)
     cv2.imshow("Aruco projection", img)
 
 
-def projectArucoPosition(marker):
+def projectArucoPosition(floor, marker):
+    markerPos = ArucoPointPosition((1222,743), 90)
     if not hasattr(projectArucoPosition, "lastPoint"):
         projectArucoPosition.lastPoint = None
-    img_size = (700, 700, 3)
-    img = np.ones(img_size) * 255
-    cv2.line(img, (300, 100), (350, 100), (0, 0, 0), 4)
+    img = cv2.imread('m2ms.png',  cv2.IMREAD_COLOR)
+    cv2.line(img, markerPos.getMarkerLine(1)[0],markerPos.getMarkerLine(1)[1] , (0, 0, 255), 5)
     if(marker != None):
-        middlePoint = (325, 100)
-        angleRadians = (m.pi / 180.0) * (marker.estimatedAngle + 90)
-        posPointX = int(middlePoint[0] + m.cos(angleRadians) * marker.estimatedDistance)
-        posPointY = int(middlePoint[1] + m.sin(angleRadians) * marker.estimatedDistance)
+        middlePoint = markerPos.getMiddlePoint()
+        angleRadians = (m.pi / 180.0) * (marker.estimatedAngle + markerPos.rotation)
+        posPointX = int(middlePoint[0] + m.cos(angleRadians) * floor.calculateSizeToPixels(marker.estimatedDistance))
+        posPointY = int(middlePoint[1] + m.sin(angleRadians) * floor.calculateSizeToPixels(marker.estimatedDistance))
         projectArucoPosition.lastPoint = (posPointX, posPointY)
         cv2.circle(img, (posPointX, posPointY), 10, (0, 255, 0), -1)
     elif(projectArucoPosition.lastPoint != None):
         cv2.circle(img, projectArucoPosition.lastPoint, 10, (0, 210, 50), -1)
 
-    cv2.imshow("Aruco position", img)
 
+    img = image_resize(img, width = 1200)
+    cv2.imshow("Display frame", img)
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
 # image = cv2.imread("1.png")
 
 
 def mainLoop():
+    floor = FloorPlan(111) #cm
     markers = []
     width = 0
     height = 0
@@ -125,10 +175,10 @@ def mainLoop():
                     marker = ArucoInfo(corners[i][0], ids[i][0])
                     markers.append(marker)
                     # print(corners[i])
-                    projectArucoMarker(width, height, marker)
+                    projectArucoMarker(width, height, floor, marker)
                 aruco.drawDetectedMarkers(image, corners, ids)
             else:
-                projectArucoMarker(width, height)
+                projectArucoMarker(width, height, floor)
             # aruco.drawDetectedMarkers(image, rejectedImgPoints, borderColor=(100, 0, 240))
 
             cv2.imshow('frame', image)
